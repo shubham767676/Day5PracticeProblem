ab69c9929202852e0693fc27042b650683344c49
“shubham767676” <shubhamv.singh06@gmail.com>
2022-02-14 22:34:04 +0530

    Added: FlipCoin

--- src/com/bridgelabz/Flipcoin.java
+++ src/com/bridgelabz/Flipcoin.java
@@ -1,44 +1,39 @@
 package com.bridgelabz;
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class Flipcoin {
 
 	public static void main(String[] args) {
-		float heads=0;
-		float tails=0;
-		float headPercentage=0;
-		float tailPercentage=0;
+		float heads = 0;
+		float tails = 0;
+		float headPercentage = 0;
+		float tailPercentage = 0;
 
-		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the number of times coin flipped  ");
-		int totalCoin = sc.nextInt();
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter the number of times coin flipped  ");
+			int totalCoin = sc.nextInt();
 
-			for(int i = 0;i < totalCoin; i++)
-			{
-				double Flipcoin = Math.random() ;
-				if (Flipcoin < 0.5) 
-				{
-					heads++ ;
+			double flipCoin;
+
+			for (int i = 0; i < totalCoin; i++) {
+				flipCoin = Math.random();
+				if (flipCoin < 0.5) {
+					heads++;
 					System.out.println("Heads");
-				}
-				else 
-				{
-					tails++ ;
+				} else {
+					tails++;
 					System.out.println("Tails");
-				}   
-				
-				headPercentage =(float)(heads/totalCoin) * 100;
-				tailPercentage = (float)(tails/totalCoin) * 100;
-
-				System.out.println("Head percentage :" +headPercentage);
-				System.out.println("Tail percentage :" +tailPercentage);
-
-				
+				}
 
 			}
+			// headPercentage = (float) (heads / totalCoin) * 100;
+			// tailPercentage = (float) (tails / totalCoin) * 100;
+
+			System.out.println(("Head percentage: " + (float) (heads / totalCoin) * 100) + "\nTail percentage: "
+					+ (float) (tails / totalCoin) * 100);
+			// System.out.println("Tail percentage :" + tailPercentage);
+		}
 
 	}
 
